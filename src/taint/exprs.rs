a4b5b12f563d81a3c8b6647010d0d4c98f4ccd2f
Diogo Correia <diogotc@kth.se>
2024-06-07 00:23:05 +0200

    refactor(ifc): move visit_operand_name into its own function

--- src/taint/exprs.rs
+++ src/taint/exprs.rs
@@ -15,41 +15,7 @@ pub fn visit_expr<'a>(
     node: &ExprNode<'a>,
 ) -> Option<LabelBacktrace<'a>> {
     match node {
-        ExprNode::Name(name) => {
-            let package = package_or_current!(context, name.package);
-            if let Some(current_symbol) = context.current_symbol() {
-                // ensure reverse dependencies are accounted for
-                if !context.symtab().is_local(package, name.id.content()) {
-                    context.add_symbol_reverse_dependency(
-                        (context.current_package(), current_symbol),
-                        (package, name.id.content()),
-                    );
-                }
-            }
-
-            let symbol = context.symtab().get_symbol(package, name.id.content());
-
-            if let Some(symbol) = symbol {
-                symbol.backtrace().as_ref().map(|symbol_backtrace| {
-                    LabelBacktrace::new(
-                        LabelBacktraceKind::Expression,
-                        context.file(),
-                        name.id.location(),
-                        Some(name.id.clone()),
-                        symbol_backtrace.label().clone(),
-                        std::iter::once(symbol_backtrace),
-                    )
-                    .unwrap() // safe since we know backtrace exists
-                })
-            } else {
-                context.report_error(AnalysisError::UnknownSymbol {
-                    file: context.file(),
-                    symbol: name.id.clone(),
-                });
-
-                None
-            }
-        }
+        ExprNode::Name(name) => visit_operand_name(context, name),
         ExprNode::Literal(_) => None,
         ExprNode::UnaryOp {
             kind: UnaryOpKind::Receive,
@@ -77,6 +43,44 @@ pub fn visit_expr<'a>(
         }
         ExprNode::Call(call) => visit_call(context, call),
         ExprNode::Indexing(indexing) => visit_indexing(context, indexing),
+    }
+}
+
+fn visit_operand_name<'a>(
+    context: &mut VisitFileContext<'a, '_>,
+    node: &OperandNameNode<'a>,
+) -> Option<LabelBacktrace<'a>> {
+    let package = package_or_current!(context, node.package);
+    if let Some(current_symbol) = context.current_symbol() {
+        // ensure reverse dependencies are accounted for
+        if !context.symtab().is_local(package, node.id.content()) {
+            context.add_symbol_reverse_dependency(
+                (context.current_package(), current_symbol),
+                (package, node.id.content()),
+            );
+        }
+    }
+
+    let symbol = context.symtab().get_symbol(package, node.id.content());
+
+    if let Some(symbol) = symbol {
+        symbol.backtrace().as_ref().and_then(|symbol_backtrace| {
+            LabelBacktrace::new(
+                LabelBacktraceKind::Expression,
+                context.file(),
+                node.id.location(),
+                Some(node.id.clone()),
+                symbol_backtrace.label().clone(),
+                std::iter::once(symbol_backtrace),
+            )
+        })
+    } else {
+        context.report_error(AnalysisError::UnknownSymbol {
+            file: context.file(),
+            symbol: node.id.clone(),
+        });
+
+        None
     }
 }
 
